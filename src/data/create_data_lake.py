738f69700215b36527515a05636bb81e728994a5
juhincapie <juhincapie@unal.edu.co>
2022-06-18 15:41:13 -0500

    Update create_data_lake.py

--- src/data/create_data_lake.py
+++ src/data/create_data_lake.py
@@ -21,6 +21,18 @@ def create_data_lake():
 
 
     """
+    import os
+    import os
+    os.mkdir('./data_lake/')
+    parent_dir = 'data_lake/'
+    carpetas = ['landing', 'raw', 'cleansed', 'business']
+    [os.mkdir(os.path.join(parent_dir, c)) for c in carpetas]
+    parent_dir = 'data_lake/business/'
+    carpetas = ['reports', 'features', 'forecasts']
+    [os.mkdir(os.path.join(parent_dir, c)) for c in carpetas]
+    parent_dir = 'data_lake/business/reports/'
+    directory = 'figures'
+    os.mkdir(os.path.join(parent_dir, directory))
     raise NotImplementedError("Implementar esta función")
 
 
