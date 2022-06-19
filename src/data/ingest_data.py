4447bef495011a8ecaa57a100af9dfefa8fe4f53
juhincapie <juhincapie@unal.edu.co>
2022-06-18 19:28:21 -0500

    Update ingest_data.py

--- src/data/ingest_data.py
+++ src/data/ingest_data.py
@@ -1,4 +1,3 @@
-# pip install wget
 def ingest_data():
     """Ingeste los datos externos a la capa landing del data lake.
 
@@ -7,20 +6,28 @@ def ingest_data():
     descarga debe realizarse usando únicamente funciones de Python.
 
     """
+    import urllib3
+    http = urllib3.PoolManager()
 
-    import wget
-    import os
+    def descargar_archivo(ruta, file_name, extension):
+        for año in file_name:
+            url_ruta = ruta + '/' + año + extension + '.xlsx?raw=true'
+            nombre_archivo = 'data_lake/landing/' + año + '.xlsx'
+            #r = urllib3.urlopen(url_ruta)
+            r = http.request('GET', url_ruta)
+            f = open(nombre_archivo, 'wb')
+            f.write(r.read())
+            f.close()
+        return
+    ruta = 'https://github.com/jdvelasq/datalabs/tree/master/datasets/precio_bolsa_nacional/xls'
+    file_name1 = [str(año) for año in range(1995, 2016)]
+    file_name2 = [str(año) for año in range(2018, 2022)]
+    file_name_xlsx = file_name1 + file_name2
+    file_name_xls = ['2016', '2017']
 
-    os.chdir("data_lake/landing/")
-    for num in range(1995, 2022):
-        if num in range(2016, 2018):
-            wdir = 'https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/{}.xls?raw=true'.format(
-                num)
-            wget.download(wdir)
-        else:
-            wdir = 'https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/{}.xlsx?raw=true'.format(
-                num)
-            wget.download(wdir)
+    descargar_archivo(ruta, file_name_xlsx, '.xlsx')
+
+    descargar_archivo(ruta, file_name_xls, '.xls')
     return
 
 
