13c115b045fd7db0cc7bfe52a2d9c6f66a817d82
juhincapie <juhincapie@unal.edu.co>
2022-06-25 09:50:29 -0500

    Update transform_data.py

--- src/data/transform_data.py
+++ src/data/transform_data.py
@@ -9,32 +9,33 @@ def transform_data():
     """
     import pandas as pd
 
-    def transformar_xls_a_csv(año, encabezado, extension):
+    def transformar_xls_a_csv(year, encabezado, extension):
         read_file = pd.read_excel(
-            "data_lake/landing/{}.{}".format(año, extension), header=encabezado)
+            "data_lake/landing/{}.{}".format(year, extension), header=encabezado)
         read_file = read_file.iloc[:, 0:25]
         read_file.columns = ['Fecha', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
                              '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23']
-        read_file.to_csv("data_lake/raw/{}.csv".format(año), index=None)
+        read_file.to_csv("data_lake/raw/{}.csv".format(year), index=None)
 
         return
 
-    for año in range(1995, 2022):
-        if año in range(1995, 2000):
-            transformar_xls_a_csv(año, 3, "xlsx")
-        elif(año in range(2000, 2016)):
-            transformar_xls_a_csv(año, 2, "xlsx")
-        elif(año in range(2016, 2018)):
-            transformar_xls_a_csv(año, 2, "xls")
+    for year in range(1995, 2022):
+        if year in range(1995, 2000):
+            transformar_xls_a_csv(year, 3, "xlsx")
+        elif(year in range(2000, 2016)):
+            transformar_xls_a_csv(year, 2, "xlsx")
+        elif(year in range(2016, 2018)):
+            transformar_xls_a_csv(year, 2, "xls")
         else:
-            transformar_xls_a_csv(año, 0, "xlsx")
-    return
+            transformar_xls_a_csv(year, 0, "xlsx")
+    # return
 
     #raise NotImplementedError("Implementar esta función")
 
 
-transform_data()
+# transform_data()
 if __name__ == "__main__":
     import doctest
 
     doctest.testmod()
+    transform_data()
