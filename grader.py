9e2c72125033d8b521dcdb162bc420df100c3ebf
juhincapie <juhincapie@unal.edu.co>
2022-06-25 18:16:13 -0500

    Update grader.py

--- grader.py
+++ grader.py
@@ -138,7 +138,7 @@ def test_10():
 def test_11():
     """Modelo creado"""
     os.system("make train_model")
-    assert os.path.isfile("models/precios-diarios.pkl") is True
+    assert os.path.isfile("models/precios-diarios.pikle") is True
 
 
 def test_12():
