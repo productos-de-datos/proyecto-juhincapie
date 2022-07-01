c1641d35abc20fe64a5df9fe572e1a1688bff4a4
juhincapie <juhincapie@unal.edu.co>
2022-06-30 21:41:52 -0500

    Update transform_data.py

--- src/data/transform_data.py
+++ src/data/transform_data.py
@@ -1,10 +1,32 @@
+"""
+Módulo de transformación de datos.
+-------------------------------------------------------------------------------
+Función ruta: De la ruta data_lake/landing/ se toma el año y la extensión de los
+archivos que se encuentran en dicha ruta
+
+Función load_data: Carga los archivos de la ruta data_lake/landing/ diferenciando la 
+columna fecha en formato YYYY-MM-DD y las horas desde H00 a H23
+
+Función save_data: Guarda los archivos en la ruta data_lake/raw/ en formato csv
+
+Función transform_data: Orquesta las funciones de ruta, load_data, save_data y transform_data 
+con el fin de de transformar a csv los archivos de la ruta data_lake/landing/ y pegarlos a la 
+ruta data_lake/raw/ con las columnas fecha en formato YYYY-MM-DD y las horas desde H00 a H23
+
+"""
+import pandas as pd
+
+
 def ruta(year, extension):
+    '''
+    >>> print(type(ruta('2021', "xlsx")))
+    <class 'str'>
+    '''
     ruta = "data_lake/landing/{}.{}".format(year, extension)
     return ruta
 
 
 def load_data(ruta, encabezado):
-    import pandas as pd
     read_file = pd.read_excel(ruta, header=encabezado)
     read_file = read_file.iloc[:, 0:25]
     read_file.columns = ['Fecha', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
@@ -17,42 +39,32 @@ def save_data(read_file, year):
 
 
 def transform_data():
-    """Transforme los archivos xls a csv.
-
-    Transforme los archivos data_lake/landing/*.xls a data_lake/raw/*.csv. Hay
-    un archivo CSV por cada archivo XLS en la capa landing. Cada archivo CSV
-    tiene como columnas la fecha en formato YYYY-MM-DD y las horas H00, ...,
-    H23.
-
-    """
-    for year in range(1995, 2022):
-        if year in range(1995, 2000):
-            out_file = ruta(year, "xlsx")
-            file = load_data(out_file, 3)
-            save_data(file, year)
-        elif(year in range(2000, 2016)):
-            out_file = ruta(year, "xlsx")
-            file = load_data(out_file, 2)
-            save_data(file, year)
-        elif(year in range(2016, 2018)):
-            out_file = ruta(year, "xls")
-            file = load_data(out_file, 2)
-            save_data(file, year)
-        else:
-            out_file = ruta(year, "xlsx")
-            file = load_data(out_file, 0)
-            save_data(file, year)
+    try:
+        for year in range(1995, 2022):
+            if year in range(1995, 2000):
+                out_file = ruta(year, "xlsx")
+                file = load_data(out_file, 3)
+                save_data(file, year)
+            elif(year in range(2000, 2016)):
+                out_file = ruta(year, "xlsx")
+                file = load_data(out_file, 2)
+                save_data(file, year)
+            elif(year in range(2016, 2018)):
+                out_file = ruta(year, "xls")
+                file = load_data(out_file, 2)
+                save_data(file, year)
+            else:
+                out_file = ruta(year, "xlsx")
+                file = load_data(out_file, 0)
+                save_data(file, year)
+    except:
+        raise NotImplementedError("Implementar esta función")
 
 
 def test_answer():
     assert ruta('2021', "xlsx") == "data_lake/landing/2021.xlsx"
 
-    # return
 
-    #raise NotImplementedError("Implementar esta función")
-
-
-# transform_data()
 if __name__ == "__main__":
     import doctest
 
