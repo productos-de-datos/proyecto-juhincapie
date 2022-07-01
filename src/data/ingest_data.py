6cfa44d194d43bc0c17a002ed214942b9bd95e98
juhincapie <juhincapie@unal.edu.co>
2022-06-30 21:41:46 -0500

    Update ingest_data.py

--- src/data/ingest_data.py
+++ src/data/ingest_data.py
@@ -1,3 +1,19 @@
+"""
+Módulo de ingestión de datos.
+-------------------------------------------------------------------------------
+De la ruta https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/ 
+se descargan los archivos de precios de bolsa nacional en formato xls y xlsx a la capa 
+data_lake/landing/
+
+Luego la función ruta_origen verifica que el datalake contenga todas las siguientes 
+carpetas requeridas: .git, .github, .gitignore, .vscode, data_lake, grader.py, Makefile, README.md, src
+
+"""
+
+import os
+import wget
+
+
 def ingest_data():
     """Ingeste los datos externos a la capa landing del data lake.
 
@@ -6,25 +22,23 @@ def ingest_data():
     descarga debe realizarse usando únicamente funciones de Python.
 
     """
-    import wget
-    import os
-
-    os.chdir("data_lake/landing/")
-    for num in range(1995, 2022):
-        if num in range(2016, 2018):
-            wdir = 'https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/{}.xls?raw=true'.format(
-                num)
-            wget.download(wdir)
-        else:
-            wdir = 'https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/{}.xlsx?raw=true'.format(
-                num)
-            wget.download(wdir)
-    os.chdir('../../')
-    #raise NotImplementedError("Implementar esta función")
+    try:
+        os.chdir("data_lake/landing/")
+        for num in range(1995, 2022):
+            if num in range(2016, 2018):
+                wdir = 'https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/{}.xls?raw=true'.format(
+                    num)
+                wget.download(wdir)
+            else:
+                wdir = 'https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/{}.xlsx?raw=true'.format(
+                    num)
+                wget.download(wdir)
+        os.chdir('../../')
+    except:
+        raise NotImplementedError("Implementar esta función")
 
 
 def test_ruta_origen():
-    import os
     assert set(os.listdir()) - set(['.git', '.github', '.gitignore',
                                     '.vscode', 'data_lake', 'grader.py', 'Makefile', 'README.md', 'src']) == set()
 
